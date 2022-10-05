e44a7a2c53bf7e2e8ce7b70676ebfe52348db3f6
Brandon They <theyb5220@gmail.com>
2022-10-05 10:41:54 -0500

    Added more stylings

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,26 @@
 import SearchBar from "../SearchBar/SearchBar"
 import "./NavBar.scss"
-
+import {GrHomeRounded} from "react-icons/gr"
+import {FaRegPaperPlane} from "react-icons/fa"
+import {BsPlusSquare} from "react-icons/bs"
+import {ImCompass2} from "react-icons/im"
+import {AiOutlineHeart} from "react-icons/ai"
+import {CgProfile} from "react-icons/cg"
 const NavBar = () => {
 
 
     return (
         <div className="navbar">
-            <h3>Instagram</h3>
-            <SearchBar />
-            {/* Icons */}
+            <h3 className="logo">Instagram</h3>
+            <SearchBar className="searchbar"/>
+            <div className="navbar-icons">
+                <GrHomeRounded className="icon"/>
+                <FaRegPaperPlane className="icon"/>
+                <BsPlusSquare className="icon"/>
+                <ImCompass2 className="icon"/>
+                <AiOutlineHeart className="icon"/>
+                <CgProfile />
+            </div>
         </div>
     )
 }
