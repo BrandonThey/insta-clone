b48b723e95bf50067efeae50ea938c3607cf2c95
Brandon They <theyb5220@gmail.com>
2022-10-05 09:26:38 -0500

    Storing user data in a file

---
+++ src/data/profiles.js
@@ -0,0 +1,15 @@
+import { profilePicture } from "../image_data/images"
+const profiles = [
+    {
+        profileName: "cats_of_instagram",
+        profilePicture: profilePicture,
+        numOfPosts: "16,889",
+        numOfFollowers: "13.1m",
+        numOfFollowing: "7",
+        bioHeader: "Cats of Instagram",
+        bioContent: "The #catsofinstagram channel. submit: hastag or link below! collab: partner@catsofinstagram.com catsofinstagram.com",
+
+    }
+]
+
+export default profiles;
